23b22d130b5fd2c81ed9ba8ec64d96fe7ca1ab34
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-13 21:33:01 -0400

    added Change/Update User to doPost of UserChangeServlet, works

--- src/main/java/com/revature/servlets/UserChangeServlet.java
+++ src/main/java/com/revature/servlets/UserChangeServlet.java
@@ -16,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Optional;
 
 @WebServlet("/change/users/*")
 public class UserChangeServlet extends HttpServlet {
@@ -59,6 +60,7 @@ public class UserChangeServlet extends HttpServlet {
                 userService.terminateUserById(id);
                 User user = userService.getUserById(id).get();
                 String userJSON = mapper.writeValueAsString(user);
+                respWriter.write(userJSON);
                 System.out.println(user);
             }
             resp.setStatus(200);
@@ -83,7 +85,6 @@ public class UserChangeServlet extends HttpServlet {
         }
 
     }
-    // todo make delete doget, make change user dopost
 
 
     @Override
@@ -94,5 +95,67 @@ public class UserChangeServlet extends HttpServlet {
         ObjectMapper mapper = new ObjectMapper();
         PrintWriter respWriter = resp.getWriter();
 
+        try{
+            String idParam = req.getParameter("id");
+            int id = Integer.parseInt(idParam);
+            User oldUser = userService.getUserById(id).get();
+            User newUser = mapper.readValue(req.getInputStream(), User.class);
+            if ( !oldUser.equals(newUser)){
+                if (!oldUser.getUsername().equals(newUser.getUsername())){
+                    if (newUser.getUsername() != null && !newUser.getUsername().trim().equals("")) {
+                        Optional<User> check = userService.getUserByUsername(newUser.getUsername());
+                        if(!check.isPresent()) {
+                            oldUser.setUsername(newUser.getUsername());
+                        }
+                    }
+                }
+                if (!oldUser.getEmail().equals(newUser.getEmail())) {
+                    if (newUser.getEmail() != null && !newUser.getEmail().trim().equals("")) {
+                        Optional<User> check = userService.getUserByEmail(newUser.getEmail());
+                        if(!check.isPresent()){
+                            oldUser.setEmail(newUser.getEmail());
+                        }
+                    }
+                }
+                if (!oldUser.getPassword().equals(newUser.getPassword())){
+                    if (newUser.getPassword() != null && !newUser.getPassword().trim().equals("")){
+                        oldUser.setPassword(newUser.getPassword());
+                    }
+                }
+                if (!oldUser.getFirstName().equals(newUser.getFirstName())){
+                    if (newUser.getFirstName() != null && !newUser.getFirstName().trim().equals("")){
+                        oldUser.setFirstName(newUser.getFirstName());
+                    }
+                }
+                if (!oldUser.getLastName().equals(newUser.getLastName())){
+                    if (newUser.getLastName() != null && !newUser.getLastName().trim().equals("")){
+                        oldUser.setLastName(newUser.getLastName());
+                    }
+                }
+                if (!oldUser.getUserRole().equals(newUser.getUserRole())){
+                    if (newUser.getUserRole() != null && !newUser.getLastName().trim().equals("")){
+                        oldUser.setUserRole(newUser.getUserRole());
+                    }
+                }
+            }
+            userService.update(oldUser);
+            String userJSON = mapper.writeValueAsString(oldUser);
+            respWriter.write(userJSON);
+            resp.setStatus(200);
+        } catch (InvalidRequestException ire){
+            resp.setStatus(400);
+            ErrorResponse err = new ErrorResponse(400, ire.getMessage());
+            respWriter.write(mapper.writeValueAsString(err));
+
+        } catch (ResourceNotFoundException rnf){
+            resp.setStatus(404);
+            ErrorResponse err = new ErrorResponse(404, rnf.getMessage());
+            respWriter.write(mapper.writeValueAsString(err));
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+            ErrorResponse err = new ErrorResponse(500, "Server Side Error");
+            respWriter.write(mapper.writeValueAsString(err));
+        }
     }
 }
