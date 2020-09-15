ba01cf74b25c4b3c883c3571aea5aee50d95e25a
Chris Yanoso <christopheryanoso@gmail.com>
2020-09-15 11:05:32 -0400

    added doPost for creating and doPut for updating in reimbursementServlet (not yet tested)

--- src/main/java/com/revature/servlets/ReimbursementServlet.java
+++ src/main/java/com/revature/servlets/ReimbursementServlet.java
@@ -2,11 +2,14 @@ package com.revature.servlets;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.exc.MismatchedInputException;
 import com.revature.dtos.ErrorResponse;
 import com.revature.dtos.Principal;
 import com.revature.exceptions.InvalidRequestException;
 import com.revature.exceptions.ResourceNotFoundException;
+import com.revature.exceptions.ResourcePersistenceException;
 import com.revature.models.Reimbursement;
+import com.revature.models.ReimbursementStatus;
 import com.revature.repos.ReimbursementRepo;
 import com.revature.services.ReimbursementService;
 
@@ -118,6 +121,129 @@ public class ReimbursementServlet extends HttpServlet {
             respWriter.write(errJSON);
         } catch (Exception e){
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setContentType("application/json");
+
+        ObjectMapper mapper = new ObjectMapper();
+        PrintWriter respWriter = resp.getWriter();
+
+        try{
+            Reimbursement newReimbursement = mapper.readValue(req.getInputStream(), Reimbursement.class);
+            reimbursementService.createNewReimbursement(newReimbursement);
+            System.out.println(newReimbursement);
+            String newReimbursementJSON = mapper.writeValueAsString(newReimbursement);
+            respWriter.write(newReimbursementJSON);
+            resp.setStatus(201);
+
+        }catch (MismatchedInputException mie){
+
+            resp.setStatus(400);
+            ErrorResponse err = new ErrorResponse(400, "Bad request: provided data can not be used");
+            String errJSON = mapper.writeValueAsString(err);
+            respWriter.write(errJSON);
+
+        } catch (ResourcePersistenceException rpe){
+
+            resp.setStatus(400);
+            ErrorResponse err = new ErrorResponse(400, rpe.getMessage());
+            String errJSON = mapper.writeValueAsString(err);
+            respWriter.write(errJSON);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            resp.setStatus(500);
+            ErrorResponse err = new ErrorResponse(500, "Server side Problem");
+            respWriter.write(mapper.writeValueAsString(err));
+        }
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        PrintWriter respWriter = resp.getWriter();
+        resp.setContentType("application/json");
+
+        String principalJSON = (String) req.getSession().getAttribute("principal");
+        System.out.println(principalJSON);
+
+        if (principalJSON == null) {
+            ErrorResponse err = new ErrorResponse(401, "No principal object found on request");
+            respWriter.write(mapper.writeValueAsString(err));
+            resp.setStatus(401);
+            return;
+        }
+        Principal principal = mapper.readValue(principalJSON, Principal.class);
+
+        try {
+            String idParam = req.getParameter("id");
+            String statusParam = req.getParameter("status");
+            if (idParam != null){
+                int id = Integer.parseInt(idParam);
+                if (principal.getRole().equalsIgnoreCase("Admin")){
+                    Reimbursement statusReimbursement = reimbursementService.getReimbursementById(id);
+                    switch(statusParam.toLowerCase()){
+                        case "approved":
+                            statusReimbursement.setReimbursementStatus(ReimbursementStatus.APPROVED);
+                            break;
+                        case "Denied":
+                            statusReimbursement.setReimbursementStatus(ReimbursementStatus.DENIED);
+                            break;
+                        case "default":
+                            break;
+                    }
+                    reimbursementService.updateReimbursement(statusReimbursement);
+                    respWriter.write(mapper.writeValueAsString(statusReimbursement));
+
+                } else {
+                    Reimbursement oldReimbursement = reimbursementService.getReimbursementById(id);
+                    Reimbursement newReimbursement = mapper.readValue(req.getInputStream(), Reimbursement.class);
+                    if (!oldReimbursement.equals(newReimbursement)){
+                        if (oldReimbursement.getAmount() != newReimbursement.getAmount()){
+                            if (newReimbursement.getAmount() > 0){
+                                oldReimbursement.setAmount(newReimbursement.getAmount());
+                            }
+                        }
+                        if (!oldReimbursement.getDescription().equals(newReimbursement.getDescription())){
+                            if (newReimbursement.getDescription() != null && newReimbursement.getDescription().trim().equals("")){
+                                oldReimbursement.setDescription(newReimbursement.getDescription());
+                            }
+                        }
+                        if (!oldReimbursement.getReimbursementType().equals(newReimbursement.getReimbursementType())){
+                            if (newReimbursement.getReimbursementType() != null && newReimbursement.getReimbursementType().toString().trim().equals("")){
+                                oldReimbursement.setReimbursementType(newReimbursement.getReimbursementType());
+                            }
+                        }
+                        reimbursementService.updateReimbursement(oldReimbursement);
+                    }
+                    String reimbursementJSON = mapper.writeValueAsString(oldReimbursement);
+                    respWriter.write(reimbursementJSON);
+                }
+                resp.setStatus(200);
+            } else {
+                ErrorResponse err = new ErrorResponse(400, "Bad Request: id Parameter needed");
+                respWriter.write(mapper.writeValueAsString(err));
+                resp.setStatus(400);
+            }
+        } catch (InvalidRequestException ire){
+            resp.setStatus(400);
+            ErrorResponse err = new ErrorResponse(400, ire.getMessage());
+            respWriter.write(mapper.writeValueAsString(err));
+
+        } catch (ResourceNotFoundException rnf){
+            resp.setStatus(404);
+            ErrorResponse err = new ErrorResponse(404, rnf.getMessage());
+            respWriter.write(mapper.writeValueAsString(err));
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+            ErrorResponse err = new ErrorResponse(500, "Server Side Error");
+            respWriter.write(mapper.writeValueAsString(err));
         }
     }
 }
--- src/main/java/com/revature/servlets/UserChangeServlet.java
+++ src/main/java/com/revature/servlets/UserChangeServlet.java
@@ -95,67 +95,73 @@ public class UserChangeServlet extends HttpServlet {
         ObjectMapper mapper = new ObjectMapper();
         PrintWriter respWriter = resp.getWriter();
 
-        try{
+        try {
             String idParam = req.getParameter("id");
             int id = Integer.parseInt(idParam);
-            User oldUser = userService.getUserById(id).get();
-            User newUser = mapper.readValue(req.getInputStream(), User.class);
-            if ( !oldUser.equals(newUser)){
-                if (!oldUser.getUsername().equals(newUser.getUsername())){
-                    if (newUser.getUsername() != null && !newUser.getUsername().trim().equals("")) {
-                        Optional<User> check = userService.getUserByUsername(newUser.getUsername());
-                        if(!check.isPresent()) {
-                            oldUser.setUsername(newUser.getUsername());
+            if (idParam != null) {
+                User oldUser = userService.getUserById(id).get();
+                User newUser = mapper.readValue(req.getInputStream(), User.class);
+                if (!oldUser.equals(newUser)) {
+                    if (!oldUser.getUsername().equals(newUser.getUsername())) {
+                        if (newUser.getUsername() != null && !newUser.getUsername().trim().equals("")) {
+                            Optional<User> check = userService.getUserByUsername(newUser.getUsername());
+                            if (!check.isPresent()) {
+                                oldUser.setUsername(newUser.getUsername());
+                            }
+                        }
+                    }
+                    if (!oldUser.getEmail().equals(newUser.getEmail())) {
+                        if (newUser.getEmail() != null && !newUser.getEmail().trim().equals("")) {
+                            Optional<User> check = userService.getUserByEmail(newUser.getEmail());
+                            if (!check.isPresent()) {
+                                oldUser.setEmail(newUser.getEmail());
+                            }
+                        }
+                    }
+                    if (!oldUser.getPassword().equals(newUser.getPassword())) {
+                        if (newUser.getPassword() != null && !newUser.getPassword().trim().equals("")) {
+                            oldUser.setPassword(newUser.getPassword());
+                        }
+                    }
+                    if (!oldUser.getFirstName().equals(newUser.getFirstName())) {
+                        if (newUser.getFirstName() != null && !newUser.getFirstName().trim().equals("")) {
+                            oldUser.setFirstName(newUser.getFirstName());
+                        }
+                    }
+                    if (!oldUser.getLastName().equals(newUser.getLastName())) {
+                        if (newUser.getLastName() != null && !newUser.getLastName().trim().equals("")) {
+                            oldUser.setLastName(newUser.getLastName());
+                        }
+                    }
+                    if (!oldUser.getUserRole().equals(newUser.getUserRole())) {
+                        if (newUser.getUserRole() != null && !newUser.getLastName().trim().equals("")) {
+                            oldUser.setUserRole(newUser.getUserRole());
                         }
                     }
                 }
-                if (!oldUser.getEmail().equals(newUser.getEmail())) {
-                    if (newUser.getEmail() != null && !newUser.getEmail().trim().equals("")) {
-                        Optional<User> check = userService.getUserByEmail(newUser.getEmail());
-                        if(!check.isPresent()){
-                            oldUser.setEmail(newUser.getEmail());
-                        }
-                    }
-                }
-                if (!oldUser.getPassword().equals(newUser.getPassword())){
-                    if (newUser.getPassword() != null && !newUser.getPassword().trim().equals("")){
-                        oldUser.setPassword(newUser.getPassword());
-                    }
-                }
-                if (!oldUser.getFirstName().equals(newUser.getFirstName())){
-                    if (newUser.getFirstName() != null && !newUser.getFirstName().trim().equals("")){
-                        oldUser.setFirstName(newUser.getFirstName());
-                    }
-                }
-                if (!oldUser.getLastName().equals(newUser.getLastName())){
-                    if (newUser.getLastName() != null && !newUser.getLastName().trim().equals("")){
-                        oldUser.setLastName(newUser.getLastName());
-                    }
-                }
-                if (!oldUser.getUserRole().equals(newUser.getUserRole())){
-                    if (newUser.getUserRole() != null && !newUser.getLastName().trim().equals("")){
-                        oldUser.setUserRole(newUser.getUserRole());
-                    }
-                }
+                userService.update(oldUser);
+                String userJSON = mapper.writeValueAsString(oldUser);
+                respWriter.write(userJSON);
+                resp.setStatus(200);
+            } else {
+                ErrorResponse err = new ErrorResponse(400, "Bad Request: id Parameter needed");
+                respWriter.write(mapper.writeValueAsString(err));
+                resp.setStatus(400);
             }
-            userService.update(oldUser);
-            String userJSON = mapper.writeValueAsString(oldUser);
-            respWriter.write(userJSON);
-            resp.setStatus(200);
-        } catch (InvalidRequestException ire){
-            resp.setStatus(400);
-            ErrorResponse err = new ErrorResponse(400, ire.getMessage());
-            respWriter.write(mapper.writeValueAsString(err));
+            } catch(InvalidRequestException ire){
+                resp.setStatus(400);
+                ErrorResponse err = new ErrorResponse(400, ire.getMessage());
+                respWriter.write(mapper.writeValueAsString(err));
 
-        } catch (ResourceNotFoundException rnf){
-            resp.setStatus(404);
-            ErrorResponse err = new ErrorResponse(404, rnf.getMessage());
-            respWriter.write(mapper.writeValueAsString(err));
-        } catch (Exception e) {
-            e.printStackTrace();
-            resp.setStatus(500);
-            ErrorResponse err = new ErrorResponse(500, "Server Side Error");
-            respWriter.write(mapper.writeValueAsString(err));
-        }
+            } catch(ResourceNotFoundException rnf){
+                resp.setStatus(404);
+                ErrorResponse err = new ErrorResponse(404, rnf.getMessage());
+                respWriter.write(mapper.writeValueAsString(err));
+            } catch(Exception e){
+                e.printStackTrace();
+                resp.setStatus(500);
+                ErrorResponse err = new ErrorResponse(500, "Server Side Error");
+                respWriter.write(mapper.writeValueAsString(err));
+            }
     }
 }
